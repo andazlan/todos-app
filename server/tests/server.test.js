3754a4b3561e576f53a28a66f9a67190b00d10c9
Andrew Azlan <azlanappleboy@gmail.com>
2018-03-28 15:27:48 +0700

    add test for user login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -212,6 +212,8 @@ describe('POST /users', () => {
                     expect(user).toExist();
                     expect(user.password).toNotBe(password);
                     done();
+                }).catch((error) => {
+                    done(error);
                 });
             });
     });
@@ -237,6 +239,51 @@ describe('POST /users', () => {
                 password : "password123"
             })
             .expect(400)
+            .end(done);
+    });
+});
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+        request(app).post('/users/login')
+            .send({
+                email : users[1].email,
+                password : users[1].password
+            })
+            .expect(200)
+            .expect((response) => {
+                expect(response.headers['x-auth']).toExist();
+            })
+            .end(done);
+            /*
+            .end((error, response) => {
+                if (error) {
+                    return done(error);
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0].body).toMatchObject({
+                        access : 'auth',
+                        token : response.headers['x-auth']
+                    });
+                    done();
+                }).catch((error) => {
+                    done(error)
+                });
+            });
+            */
+    });
+
+    it('should reject invalid login', (done) => {
+        request(app).post('/users/login')
+            .send({
+                email : users[1].email + "asdasd",
+                password : users[1].password + "sadasd"
+            })
+            .expect(400)
+            .expect((response) => {
+                expect(response.headers['x-auth']).toNotExist();
+            })
             .end(done);
     });
 });
