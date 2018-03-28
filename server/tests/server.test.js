4f75558cf46e434aeecc69143da20dd7c3a7c0d8
Andrew Azlan <azlanappleboy@gmail.com>
2018-03-28 16:03:29 +0700

    Add tests for delete token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -255,6 +255,7 @@ describe('POST /users/login', () => {
                 expect(response.headers['x-auth']).toExist();
             })
             .end(done);
+            //FIXME: the test should be like this but i don't know it doesn't work
             /*
             .end((error, response) => {
                 if (error) {
@@ -285,5 +286,26 @@ describe('POST /users/login', () => {
                 expect(response.headers['x-auth']).toNotExist();
             })
             .end(done);
+    });
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((error, response) => {
+                if (error) {
+                    return done(error);
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((error) => {
+                    done(error);
+                });
+            });
     });
 });
